0f2b7a590de7143ee798384c23b7de32d50ed09e
yaq1n0 <62781980+yaq1n0@users.noreply.github.com>
2025-04-27 02:58:21 +0100

    Initial Commit

---
+++ TrayRunner/TrayRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrayRunnerApp.swift
+//  TrayRunner
+//
+//  Created by Yaqin Hasan on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrayRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
